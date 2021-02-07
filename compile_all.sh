5c7a7fa2b8cf438694be1d1e6de669bd1b0383ec
Matheus Vieira <matheus.sousav@gmail.com>
2021-02-07 20:18:52 +0100

    Fix merge

--- compile_all.sh
+++ compile_all.sh
@@ -1,8 +1,4 @@
 echo "Compiling LSQR_CPU"
 g++ -o LSQR_CPU -I lsqr_cpu/eigen-3.3.7 lsqr_cpu/lsqr_cpu.cpp -std=c++11
 echo "Compiling LSQR_GPU"
-<<<<<<< HEAD
 nvcc -o LSQR_GPU lsqr_gpu/matmul/GPUVector.cu lsqr_gpu/matmul/SpMat.cu lsqr_gpu/main.cu -std=c++11 -lcublas -lcusparse
-=======
-nvcc -o LSQR_GPU lsqr_gpu/matmul/GPUVector.cu lsqr_gpu/matmul/SpMat.cu lsqr_gpu/main.cu -std=c++11 -lcublas
->>>>>>> cab6b29a8e2e5efb0f8f7a374e68ba5a8b3d9ee2
